ab6a41081ac6211d4f46a18828e66146bf6218b8
Sven Marnitz <marnitz.sven@gmail.com>
2016-07-10 11:17:03 +0200

    WIP

--- crons/30min.js
+++ crons/30min.js
@@ -1,9 +1,26 @@
 'use strict';
 const _ = require('lodash');
+const commandLineArgs = require('command-line-args');
 
+const RedisCache = require('../lib/cache/redis');
 const AbstractCron = require('./abstract');
 
-class Cron15Min extends AbstractCron {
+const options = commandLineArgs([{
+  name: 'SLACK_API_TOKEN',
+  type: String,
+  defaultValue: ''
+}, {
+  name: 'CORP_KEY_ID',
+  type: String,
+  defaultValue: ''
+}, {
+  name: 'CORP_VCODE',
+  type: String,
+  defaultValue: ''
+}]);
+const cache = new RedisCache();
+
+class Cron30Min extends AbstractCron {
 
   constructor() {
     super();
@@ -11,11 +28,14 @@ class Cron15Min extends AbstractCron {
 
   wallet() {
     console.log(this.XmlClient);
+    console.log(options);
   }
 
 }
 
-const cron = new Cron15Min();
+const cron = new Cron30Min();
 cron.wallet();
 
 process.exit();
+
+// SLACK_API_TOKEN=xoxb-43193264949-n8PJGlNmp0iHeMSBJCBL0KWG CORP_KEY_ID=2924405 CORP_VCODE=zd6XGV82MfiFs9KsIVUzjiXHmzNIvvA5AruDKGOfhltaQjKtO1LTX2UdiHNgob9X
